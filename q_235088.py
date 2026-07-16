0a448f466b2afebdd345459fd3e4f6845813faf1
khosraviTech <khosravitech@gmail.com>
2026-07-16 16:35:07 +0330

    first commit

---
+++ q_235088.py
@@ -0,0 +1,20 @@
+# https://quera.org/problemset/235088
+
+# input
+n = int(input())
+lr=input()
+
+l=int(lr.split(' ')[0])
+r=int(lr.split(' ')[1])
+
+# algorithem
+min_day=n//l
+remain_day=n%l
+i=0
+while(i!=n):
+    
+    pass
+
+if(remain_day!=0):
+    if(remain_day+l<=r):
+        pass
