ed8ee5fe97c84ea143d68ce2acd573cf0b18f0d7
khosraviTech <khosravitech@gmail.com>
2026-07-16 17:13:02 +0330

    add 4th if logic.

--- q_235088.py
+++ q_235088.py
@@ -20,13 +20,15 @@ work_hight = ceiling - floor
 for_ceiling = day_on_ceiling + remain_day_on_ceiling
 for_floor = day_on_floor + remain_day_on_floor
 
-if((work_hight==0) and remain_day_on_ceiling == 0 ): #7 3,3
+if((work_hight==0) and remain_day_on_ceiling == 0 ): #7 3,3 OK
     print(day_on_ceiling)
 
-if((for_ceiling < for_floor) and remain_day_on_ceiling == 0 ): #4 1,2
+if((for_ceiling < for_floor) and remain_day_on_ceiling == 0 ): #4 1,2 OK
     print(day_on_ceiling)
 
-if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10 3,6
+if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10 3,6 OK
     print(day_on_ceiling+1)
 
-    
+#10 3,4?    
+# if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10 3,6 OK
+#     print(day_on_ceiling+1)
