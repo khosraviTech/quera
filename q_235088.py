12b8819e5c444ddfcf8cbd4f878f06dabde37915
khosraviTech <khosravitech@gmail.com>
2026-07-17 11:01:44 +0330

    add 5th if

--- q_235088.py
+++ q_235088.py
@@ -15,12 +15,12 @@ remain_day_on_floor = n % floor
 day_on_ceiling = n // ceiling
 remain_day_on_ceiling = n % ceiling
 
-work_hight = ceiling - floor
+hight = ceiling - floor
 
 for_ceiling = day_on_ceiling + remain_day_on_ceiling
 for_floor = day_on_floor + remain_day_on_floor
 
-if((work_hight==0) and remain_day_on_ceiling == 0 ): #7 3,3 OK
+if((hight==0) and remain_day_on_ceiling == 0 ): #7 3,3 OK
     print(day_on_ceiling)
 
 if((for_ceiling < for_floor) and remain_day_on_ceiling == 0 ): #4 1,2 OK
@@ -29,6 +29,5 @@ if((for_ceiling < for_floor) and remain_day_on_ceiling == 0 ): #4 1,2 OK
 if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10 3,6 OK
     print(day_on_ceiling+1)
 
-#10 3,4?    
-# if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10 3,6 OK
-#     print(day_on_ceiling+1)
+elif( remain_day_on_floor <= hight and n==(day_on_floor-1)*floor+ceiling  ): #10 3,4 OK
+    print(day_on_floor-1+1)
