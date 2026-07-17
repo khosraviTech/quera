884d103ff385c832a98ab5a600e67396a5c21c32
khosraviTech <khosravitech@gmail.com>
2026-07-17 11:45:02 +0330

    add new algorithem. TODO:impliment algorithem.

--- q_235088.py
+++ q_235088.py
@@ -31,3 +31,37 @@ if((for_ceiling < for_floor) and floor < remain_day_on_ceiling < ceiling  ): #10
 
 elif( remain_day_on_floor <= hight and n==(day_on_floor-1)*floor+ceiling  ): #10 3,4 OK
     print(day_on_floor-1+1)
+
+
+
+# new algorithem:
+
+day_on_floor = n // floor
+remain_day_on_floor = n % floor
+
+day_on_ceiling = n // ceiling
+remain_day_on_ceiling = n % ceiling
+
+hight = ceiling - floor
+# 22 3,4
+def dividing(n=n,floor=floor,ceiling=ceiling,hight=hight,day_on_ceiling=day_on_ceiling,remain_day_on_ceiling=remain_day_on_ceiling)-> int:   
+    
+    if(n<floor or (hight==0 and remain_day_on_ceiling!=0)):
+        return -1
+
+    if(hight==0 and remain_day_on_ceiling==0):
+        return day_on_ceiling
+    
+    if(remain_day_on_ceiling > ceiling):
+
+        if(dividing(n=remain_day_on_ceiling)== -1):
+
+            day_on_ceiling = day_on_ceiling -1
+            dividing(day_on_ceiling=day_on_ceiling, remain_day_on_ceiling = n - (day_on_ceiling*ceiling))
+
+        elif():
+            pass
+    elif(remain_day_on_ceiling < ceiling):
+        pass
+
+
