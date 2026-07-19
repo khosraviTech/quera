4bf7929c7dd5a633156434ad991b3120f7843f8e
khosraviTech <khosravitech@gmail.com>
2026-07-19 11:07:57 +0330

    fixed: algorithem and  input type.

--- q_218361.py
+++ q_218361.py
@@ -1,12 +1,15 @@
 # https://quera.org/problemset/218361
 
 # input
-row_1=list(int(input().split(' ')))
-row_2=list(int(input().split(' ')))
+row_1=list(input().split(' '))
+row_2=list(input().split(' '))
 
 # algorithem
 counter=0
 
-for i in row_1:
-    if(row_1[i]==1 and row_1[i]==row_2[0]):
+for i in range(8):
+    if(row_1[i]=='1' and row_1[i]==row_2[i]):
         counter+=1
+
+# output
+print(counter)
