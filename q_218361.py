fc02c4a47d33f330f6e7e4ccfe6298a2668f362c
khosraviTech <khosravitech@gmail.com>
2026-07-19 11:01:59 +0330

    add algorithem.

--- q_218361.py
+++ q_218361.py
@@ -1,4 +1,12 @@
 # https://quera.org/problemset/218361
+
 # input
 row_1=list(int(input().split(' ')))
 row_2=list(int(input().split(' ')))
+
+# algorithem
+counter=0
+
+for i in row_1:
+    if(row_1[i]==1 and row_1[i]==row_2[0]):
+        counter+=1
