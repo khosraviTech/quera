e6d7b69f27d69bc757dcbda36e6782ed58e90714
khosraviTech <khosravitech@gmail.com>
2026-07-21 09:34:11 +0330

    add algorithm.

---
+++ q_2529.py
@@ -0,0 +1,15 @@
+# https://quera.org/problemset/2529
+
+# input
+names = list()
+n = int(input())
+for i in range(n):
+    names.append(input())
+
+# algorithm
+names_dict = dict()
+
+for i in names:
+    names_dict.update({i: len(set(i))})
+
+
