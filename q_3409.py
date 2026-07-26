6a158e3b84606f3881c0ba31f78e31e55b544405
khosraviTech <khosravitech@gmail.com>
2026-07-26 10:32:27 +0330

    add algorithm & output

--- q_3409.py
+++ q_3409.py
@@ -18,4 +18,15 @@ for i in range(1, n + 1):  # init
     horizontal.append(i)
     vertical.append(i)
 
+for i in range(1, len(horizontal) + 1):  # first line
+    if i == len(horizontal):
+        print(i)
+        break
+    print(i,'',end='')
 
+for v in range(2, len(vertical) + 1): # 2 3 4 5
+    for h in horizontal:# 1  2 3 4 5
+        if h == len(horizontal):
+                print(h*v)
+                break 
+        print(h*v,'',end='')
