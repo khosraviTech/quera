dfe57f63b96a6ee51494f58ff57519ec34707a72
khosraviTech <khosravitech@gmail.com>
2026-08-06 09:40:31 +0330

    add algorithm

--- q_76278.py
+++ q_76278.py
@@ -1,9 +1,30 @@
 # https://quera.org/problemset/76278
 
 # input
-n = int(input())
-m = int(input())
-li: list[int] = list(map(int, (input().split(" "))))
-print("n is:", n, "m is:", m, "li is:", li)
+
+# n = int(input())
+# m = int(input())
+# li: list[int] = list(map(int, (input().split(" "))))
+# print("n is:", n, "m is:", m, "li is:", li)
+
+# algorithm
+def calculator(n, m, li):
+
+    sep_list = [li[i : i + m] for i in range(0, len(li), m)]
+    # print("sep list is: ", sep_list)
+    sum_list = []
+    for i in sep_list:
+        sum_list.append(sum(i))
+    # print("sum list is:", sum_list)
+
+    for i in range(0, len(sum_list)):
+        if i % 2 != 0:
+            sum_list[i] = -abs(sum_list[i])
+
+    # print("sum lsit after negativation:", sum_list)
+    # print("sum of sum_list values and the final result is : ", sum(sum_list))
+    
+    return sum(sum_list)
 
 
+# calculator(n, m, li)
