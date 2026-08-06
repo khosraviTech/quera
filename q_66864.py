2fcf2dd9f1ab57af4ba0ab5a907ebdc5af2f69b2
khosraviTech <khosravitech@gmail.com>
2026-08-06 09:39:02 +0330

    add input.

--- q_66864.py
+++ q_66864.py
@@ -11,3 +11,4 @@ for i in range(1, k + 1):
 
 # output
 print(number[k - 1])
+
---
+++ q_76278.py
@@ -0,0 +1,9 @@
+# https://quera.org/problemset/76278
+
+# input
+n = int(input())
+m = int(input())
+li: list[int] = list(map(int, (input().split(" "))))
+print("n is:", n, "m is:", m, "li is:", li)
+
+
