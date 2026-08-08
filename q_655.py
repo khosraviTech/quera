8c57edee134fc497037e8966099d98c28e44a43a
khosraviTech <khosravitech@gmail.com>
2026-08-08 15:15:08 +0330

    add algorithm

--- q_655.py
+++ q_655.py
@@ -2,3 +2,15 @@
 
 # input
 n = int(input())
+
+
+# algorithm
+for _ in range(n):
+    movie = input()
+    words = movie.split()
+
+    result = []
+
+    for word in words:
+        word = word[0].upper() + word[1:].lower()
+        result.append(word)
